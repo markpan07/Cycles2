a5baed8efb7a3129253dfd709dce8178ad631994
МАРК <markpan07@mail.ru>
2023-01-14 16:04:30 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,53 @@
+public class Main {
+    public static void main(String[] args) {
+        task1();
+        System.out.println();
+        task2();
+        System.out.println();
+        task3();
+        System.out.println();
+        task4();
+        System.out.println();
+        task5();
+        System.out.println();
+        task6();
+        System.out.println();
+        task7();
+        System.out.println();
+        task8();
+        System.out.println();
+
+    }
+
+    public static void task1() {
+
+    }
+
+    public static void task2() {
+
+    }
+
+    public static void task3() {
+
+    }
+
+    public static void task4() {
+
+    }
+
+    public static void task5() {
+
+    }
+
+    public static void task6() {
+
+    }
+
+    public static void task7() {
+
+    }
+
+    public static void task8() {
+
+    }
+}
